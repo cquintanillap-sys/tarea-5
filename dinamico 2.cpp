b70977bcbe801d9ad7a1967cbf295df893b51cf7
Cristian Quintanilla <cquintanillap@miumg.edu.gt>
2026-04-25 18:57:30 -0600

    primer ejercicio

---
+++ dinamico 2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+main(){
+	int total = 0;
+	int notas[total];
+	char res;
+	
+	do{
+		cout<<"Desea ingresar otro valor (s/n):";
+		cin>>res;
+	}while(res=='s' || res== 'S');
+	
+	system("pause");
+}
